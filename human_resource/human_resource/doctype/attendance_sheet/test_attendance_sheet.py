dd675ae570878a9dc5a22f98d27b6c93f0717ad2
reddy <reddymeghraj@gmail.com>
2015-04-06 12:10:20 +0530

    [human resource for medical Equipment]

---
+++ human_resource/human_resource/doctype/attendance_sheet/test_attendance_sheet.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2013, wayzontech and Contributors
+# See license.txt
+
+import frappe
+import unittest
+
+test_records = frappe.get_test_records('Attendance Sheet')
+
+class TestAttendanceSheet(unittest.TestCase):
+	pass
